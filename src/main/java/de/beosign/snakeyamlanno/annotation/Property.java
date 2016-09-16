bd8ced1a3ed1a5d01cfba85f633cf19c50d2e8bb
Florian Dahlmanns <florian.dahlmanns@googlemail.com>
2016-09-17 00:01:21 +0200

    Simplify property annotation to contain only 'key'

--- src/main/java/de/beosign/snakeyamlanno/annotation/Property.java
+++ src/main/java/de/beosign/snakeyamlanno/annotation/Property.java
@@ -1,20 +1,24 @@
 package de.beosign.snakeyamlanno.annotation;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Defines an alias for a property. The key must match the key used in the YAML file. The annotation must be placed on the field or getter that is responsible
+ * for storing the value found under the given key.
+ * 
+ * @author florian
+ */
 @Target({ METHOD, FIELD })
 @Retention(RUNTIME)
 public @interface Property {
+    /**
+     * The key as used in the YAML file.
+     * 
+     * @return key
+     */
     String key();
-
-    String readMethod() default "";
-
-    String writeMethod() default "";
-
-    Class<?> beanClass() default Object.class;
 }
