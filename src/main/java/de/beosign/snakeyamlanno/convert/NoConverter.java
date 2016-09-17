6359e659f43919546e910b967bebb5254b24ad9b
Florian Dahlmanns <florian.dahlmanns@googlemail.com>
2016-09-17 15:32:33 +0200

    Convert NoConverter to an interface

--- src/main/java/de/beosign/snakeyamlanno/convert/NoConverter.java
+++ src/main/java/de/beosign/snakeyamlanno/convert/NoConverter.java
@@ -1,22 +1,9 @@
 package de.beosign.snakeyamlanno.convert;
 
-import org.yaml.snakeyaml.nodes.Node;
-
 /**
- * Class that is used as the default value in the Property annotation.
+ * Interface that is used as the default value in the Property annotation.
  * 
  * @author florian
  */
-public class NoConverter implements Converter<Object> {
-
-    @Override
-    public String convertToYaml(Object modelValue) {
-        throw new IllegalStateException("Must not be called!");
-    }
-
-    @Override
-    public Object convertToModel(Node yamlNode) {
-        throw new IllegalStateException("Must not be called!");
-    }
-
+public interface NoConverter extends Converter<Object> {
 }
