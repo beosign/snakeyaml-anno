58f0ece34be57d9241f70617b8c01f6983d157a1
Florian Dahlmanns <florian.dahlmanns@googlemail.com>
2017-02-21 22:04:37 +0100

    replaces the property name with its alias while dumping

--- src/main/java/de/beosign/snakeyamlanno/representer/AnnotationAwareRepresenter.java
+++ src/main/java/de/beosign/snakeyamlanno/representer/AnnotationAwareRepresenter.java
@@ -4,16 +4,26 @@ import java.beans.IntrospectionException;
 import java.util.Iterator;
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.yaml.snakeyaml.introspector.Property;
 import org.yaml.snakeyaml.nodes.NodeTuple;
+import org.yaml.snakeyaml.nodes.ScalarNode;
 import org.yaml.snakeyaml.nodes.Tag;
 import org.yaml.snakeyaml.representer.Representer;
 
 import de.beosign.snakeyamlanno.AnnotationAwarePropertyUtils;
 import de.beosign.snakeyamlanno.property.AnnotatedProperty;
 
+/**
+ * Representer that is aware of annotations.
+ * 
+ * @author florian
+ */
 public class AnnotationAwareRepresenter extends Representer {
 
+    /**
+     * Sets the {@link AnnotationAwarePropertyUtils} into this representer.
+     */
     public AnnotationAwareRepresenter() {
         setPropertyUtils(new AnnotationAwarePropertyUtils());
     }
@@ -40,7 +50,15 @@ public class AnnotationAwareRepresenter extends Representer {
 
     @Override
     protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue, Tag customTag) {
-        // TODO Auto-generated method stub
+        NodeTuple nodeTuple = super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
+
+        if (property instanceof AnnotatedProperty) {
+            AnnotatedProperty annotatedProperty = (AnnotatedProperty) property;
+            if (StringUtils.isNotBlank(annotatedProperty.getPropertyAnnotation().key())) {
+                ScalarNode keyNode = (ScalarNode) representData(annotatedProperty.getPropertyAnnotation().key());
+                return new NodeTuple(keyNode, nodeTuple.getValueNode());
+            }
+        }
         return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
     }
 
---
+++ src/test/java/de/beosign/snakeyamlanno/representer/DumpPropertyAliasTest.java
@@ -0,0 +1,40 @@
+package de.beosign.snakeyamlanno.representer;
+
+import static org.hamcrest.core.StringContains.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import de.beosign.snakeyamlanno.property.StellarObject;
+
+/**
+ * Tests that aliasing properties works.
+ */
+public class DumpPropertyAliasTest {
+
+    /**
+     * Tests aliasing on a getter and on a field.
+     */
+    @Test
+    public void testAlias() {
+        StellarObject stellarObject = new StellarObject();
+        stellarObject.setAbsoluteMag(1.0);
+        stellarObject.setDistance("3km");
+        stellarObject.setLocation("here");
+        stellarObject.setName("my name");
+        stellarObject.setRadius(4);
+        stellarObject.setType("my type");
+
+        Yaml yaml = new Yaml(new AnnotationAwareRepresenter());
+        String dumped = yaml.dumpAsMap(stellarObject);
+
+        assertThat(dumped, containsString("distance: 3km"));
+        assertThat(dumped, containsString("location: here"));
+        assertThat(dumped, containsString("radius: 4"));
+        assertThat(dumped, containsString("type: my type"));
+        assertThat(dumped, containsString("absMag: 1.0"));
+        assertThat(dumped, containsString("nameAlias: my name"));
+    }
+
+}
