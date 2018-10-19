9f943f98be5a14c110890c5de2f761f3f82ad401
Florian Dahlmanns <florian.dahlmanns@googlemail.com>
2018-10-19 17:06:15 +0200

    YMLANNO-5: add a DefaultConstructor implementation of a CustomConstructor

---
+++ src/main/java/de/beosign/snakeyamlanno/constructor/DefaultConstructor.java
@@ -0,0 +1,28 @@
+package de.beosign.snakeyamlanno.constructor;
+
+import java.util.function.Function;
+
+import org.yaml.snakeyaml.error.YAMLException;
+import org.yaml.snakeyaml.nodes.Node;
+
+/**
+ * A default constructor just constructs an object by using the passed in defaultConstructor instance.
+ * 
+ * @author florian
+ */
+public class DefaultConstructor implements CustomConstructor<Object> {
+
+    /**
+     * Constructs an object by just calling <code>defaultConstructor.apply</code>.
+     * 
+     * @param node node
+     * @param defaultConstructor default constructor
+     * @return constructed object
+     * @throws YAMLException if an exception occurs during construction
+     */
+    @Override
+    public Object construct(Node node, Function<? super Node, ?> defaultConstructor) throws YAMLException {
+        return defaultConstructor.apply(node);
+    }
+
+}
